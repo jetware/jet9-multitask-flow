3b473fbd2b19e06e6b490e8f3838eda8ef4590ef
Cyril Vechera <cv@serversnab.ru>
2015-10-05 00:28:02 +0300

    v1.3.0 prerelease
    * multiples directories in INIT_DIR
    * tasks with absolute pathes in CLI arguments
    * loading program in library-mode (only funcions)
    * new examples
    * dashes and dots support for task names

---
+++ examples/freebsd/freebsd-multitask-init.sh
@@ -0,0 +1,39 @@
+# Get dependencies for rcorder-compatible scripts
+
+task_dependencies() {
+	local task=$1 action=$2 relation=$3
+	local script dependency dependencies
+	task_dependencies_result=
+	eval script=\$path_$task
+
+	if [ -e $script ]; then
+		case "$action" in
+		start|faststart|quietstart)
+			case "$relation" in
+			wait) section="REQUIRE" ;;
+			lead) section="BEFORE" ;;
+			mark) section="PROVIDE" ;;
+			esac
+			;;
+		stop|faststop)
+			case "$relation" in
+			wait) section="BEFORE" ;;
+			lead) section="REQUIRE" ;;
+			mark) section="PROVIDE" ;;
+			esac
+			;;
+		esac
+
+		dependencies=`awk '/^# '$section': /{for (i=3; i<=NF; ++i) { print $i }}' $script`
+		for dependency in $dependencies; do
+			task_internal $dependency
+			task_dependencies_result="$task_dependencies_result $task_internal_result"
+		done
+	fi
+}
+
+# Get script path by task name and pass the path and the action to run_rc_script
+task_execute() {
+	eval run_rc_script \$path_$1 $2
+}
+
